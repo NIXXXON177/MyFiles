4e154000442e3ebebccd631baaf5f778530f1c85
NIXXXON177 <kudrasovn024@gmail.com>
2025-10-09 09:29:55 +0300

    Commit

--- Codes/YPKartseva/React/react-app/.hygen/create/component/component.jsx.ejs.t
+++
@@ -1,9 +0,0 @@
----
-to: <%= absPath %>/<%= component_name %>.jsx
----
-import React from 'react'
-import style from './<%= component_name %>.module.css'
-
-export const <%= component_name %> = props => {
-    return <div className={styles.container}></div>
-}
--- Codes/YPKartseva/React/react-app/src/App.jsx
+++ Codes/YPKartseva/React/react-app/src/App.jsx
@@ -1,43 +1,12 @@
 import React from 'react'
-import './App.css'
-import { generate as randomWords } from 'random-words';
-import ComponentClass from "./components/ComponentClass/ComponentClass"
-import PureComponentClass from "./components/PureComponentClass/PureComponentClass"
-import ComponentFunc from "./components/ComponentFunc"
 
-export default class App extends React.Component {
-  state = {
-    count: 0,
-    str: 'str',
-    pure: 'pure',
-    func: 'func'
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      if (this.state.count % 2) {
-        this.setState({
-          count: this.state.count + 1,
-          str: randomWords()
-        })
-      } else {
-        this.setState({
-          count: this.state.count + 1,
-          str: randomWords(),
-          pure: randomWords(),
-          func: randomWords(),
-        })
-      }
-    }, 3000)
-  }
-  render(){
-    console.clear()
+function App() {
     return (
-    <header className='App-header'>
-      <ComponentClass string={this.state.str}/>
-      <PureComponentClass string={this.state.pure}/>
-      <ComponentFunc string={this.state.func}/>
-    </header>
-  )
-  }
+        <>
+            <header/>
+            <main/>
+        </>
+    )
 }
+
+export default App;
