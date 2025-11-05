6243d1d32331734981bae341fdda958e04a600fa
NIXXXON177 <kudrasovn024@gmail.com>
2025-11-05 11:21:40 +0300

    CKEditor

--- Codes/Laravel/laravel9/resources/views/posts/parts/form.blade.php
+++ Codes/Laravel/laravel9/resources/views/posts/parts/form.blade.php
@@ -2,8 +2,19 @@
 <input type="text" name="title" id="title" class="form-control" placeholder="Заголовок" required value="{{ old('title') ?? $post->title ?? ''}}">
 </div>
 <div class="form-group">
-<textarea name="descr" class="form-control" rows="7" placeholder="Описание" required>{{ old('descr') ?? $post->descr ?? ''}}</textarea>
+<textarea id="descr" name="descr" class="form-control" rows="7" placeholder="Описание" required>{{ old('descr') ?? $post->descr ?? ''}}</textarea>
 </div>
 <div class="form-group">
 <input type="file" name="img" id="img" class="form-control">
 </div>
+
+<script src="https://cdn.ckeditor.com/ckeditor5/41.0.0/classic/ckeditor.js"></script>
+<script>
+ClassicEditor
+    .create(document.querySelector('#descr'), {
+        language: 'ru'
+    })
+    .catch(error => {
+        console.error(error);
+    });
+</script>
--- Codes/Laravel/laravel9/resources/views/posts/show.blade.php
+++ Codes/Laravel/laravel9/resources/views/posts/show.blade.php
@@ -6,11 +6,12 @@
             <div class="card">
                 <div class="card-header"><h2>{{ $post->title }}</h2></div>
                 <div class="card-body">
-                    <div class="card-img card-img__max" style="background-image: url('{{ $post->img ?: asset('img/image.png') }}')"></div>
-                    <div class="card-descr">Описание: {{$post->descr}}</div>
+                    <div class="card-img card-img__max" style="background-image: url('{{ $post->img ?: asset("img/image.png") }}')"></div>
+                    <div class="card-descr">Описание: {!! $post->descr !!}</div>
                     <div class="card-author">Автор: <b>{{ $post->author_name }}</b></div>
                     <div class="card-date">Пост создан: <b>{{ $post->created_at->diffForHumans() }}</b></div>
                     <div class="card-btn">
+
                         <a href="{{ route('post.index') }}" class="btn btn-outline-primary">На главную</a>
                         @auth
                         @if(Auth::user()->id == $post->author_id)
